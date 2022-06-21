9227b33176fe8d35edab541d7cd0c23455b7b016
antaninazhuk <101988172+antaninazhuk@users.noreply.github.com>
2022-06-21 21:58:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Task 1
+
+// function Person(userName, age, id) {
+//     this.userName = userName,
+//         this.age = age,
+//         this.id = id
+// }
+
+// proto = {
+//     sayHello() {
+//         console.log('Hello from prototype');
+//     }
+// }
+
+// Person.prototype = proto;
+
+// Task 2
+
+// fruit = {
+//     isTasty: true,
+
+// }
+// orange = {
+//     form: 'round',
+//     price: 5,
+//     __proto__: fruit
+
+// }
+
+
+// apple = {
+//     color: 'red',
+//     __proto__: orange
+
+// }
+
+
+// Task 3
+
+// proto = {
+//     sayHi() {
+//         console.log('Hi');
+//     }
+// }
+
+// myObject = Object.create(proto);
+// console.log(Object.getPrototypeOf(myObject));
